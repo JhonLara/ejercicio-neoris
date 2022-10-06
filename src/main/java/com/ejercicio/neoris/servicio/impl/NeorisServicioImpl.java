f848a08d1e959536d0138a47e30ce27b52ada3b7
JhonLara <jhonalexlara10@hotmail.com>
2022-10-06 16:21:13 -0500

    Se documenta codigo del servicio

--- src/main/java/com/ejercicio/neoris/servicio/impl/NeorisServicioImpl.java
+++ src/main/java/com/ejercicio/neoris/servicio/impl/NeorisServicioImpl.java
@@ -68,6 +68,9 @@ public class NeorisServicioImpl implements NeorisServicio {
 	public void crearCuentas(List<Cuenta> cuentas) {
 		cuentas.forEach((cuenta) -> {
 			if (cuenta.getId() != null) {
+				/**
+				 * Se valida si ya existe y se actualiza
+				 */
 				Optional<Cuenta> optCuenta = cuentaRepositorio.findById(cuenta.getId());
 				Cuenta cuentaActualizar = optCuenta.get();
 				cuentaRepositorio.save(cuentaActualizar);
@@ -81,14 +84,28 @@ public class NeorisServicioImpl implements NeorisServicio {
 	public void crearMovimientos(List<Movimiento> movimientos) throws Exception{
 		movimientos.forEach((movimiento) -> {
 			if (movimiento.getId() != null) {
+				/**
+				 * Se valida si ya existe y se actualiza
+				 */
 				Optional<Movimiento> optMovimiento = movimientoRepositorio.findById(movimiento.getId());
 				Movimiento movimientoActualizar = optMovimiento.get();
 				movimientoRepositorio.save(movimientoActualizar);
 			} else {
 				Cuenta cuenta = cuentaRepositorio.findById(movimiento.getCuenta().getId()).get();
+				/**
+				 * Se busca la cuenta del movimiento
+				 */
 				movimiento.setTipo(movimiento.getValor() < 0 ? "Retiro" : "Deposito");
+				/**
+				 * Se valida el saldo final
+				 */
 				Long operacion = cuenta.getSaldoActual() == null ? cuenta.getSaldoInicial()
 						: cuenta.getSaldoActual() + movimiento.getValor();
+				/**
+				 * Se valida si se puede o no hacer el movimiento
+				 * 
+				 * De no poderse, se lanza exception, de poderse se actualiza los saldos y se realiza movmiento
+				 */
 				if (movimiento.getTipo().equals("Retiro") && (cuenta.getSaldoInicial() == 0 || operacion < 0)) {
 					cuenta.setSaldoActual(cuenta.getSaldoInicial());
 					throw new RuntimeException("Saldo no disponible");
@@ -107,6 +124,9 @@ public class NeorisServicioImpl implements NeorisServicio {
 	public void crearClientes(List<Cliente> clientes) {
 		clientes.forEach((cliente) -> {
 			if (cliente.getId() != null) {
+				/**
+				 * Se valida si ya existe y se actualiza
+				 */
 				Optional<Cliente> optCliente = clienteRepositorio.findById(cliente.getId());
 				Cliente clienteActualizar = optCliente.get();
 				clienteRepositorio.save(clienteActualizar);
@@ -120,6 +140,9 @@ public class NeorisServicioImpl implements NeorisServicio {
 	public List<ReporteDTO> obtenerReporte(LocalDate fechaInicio, LocalDate fechaFin, Long idCliente) {
 		List<ReporteDTO> reporte = new ArrayList<>();
 		List<Movimiento> movimientos = movimientoRepositorio.findAll();
+		/**
+		 * Se valida la lista de movimientos para consolidar informacion del reporte
+		 */
 		movimientos.stream().filter(m -> m.getFecha().isBefore(fechaFin) && m.getFecha().isAfter(fechaInicio)
 				&& m.getCuenta().getCliente().getId() == idCliente).forEach((movimiento) -> {
 					System.out.println("entra " + movimiento.toString());
